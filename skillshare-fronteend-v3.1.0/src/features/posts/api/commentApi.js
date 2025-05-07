5a36aedabb7480109f9ea3fa48e69413eee3ac8b
Amasha gamage <it22036070@my.sliit.lk>
2025-05-07 20:50:29 +0530

    CommentApiUpdate

--- skillshare-fronteend-v3.1.0/src/features/posts/api/commentApi.js
+++ skillshare-fronteend-v3.1.0/src/features/posts/api/commentApi.js
@@ -15,4 +15,18 @@ export default {
     }
   },
 
- 
+  // Update an existing comment
+  async updateComment(commentId, userId, commentData) {
+    try {
+      const response = await apiClient.put(`/comments/${commentId}`, commentData);
+      return response.data;
+    } catch (error) {
+      let errorMessage = "Failed to update comment";
+      if (error.response) {
+        errorMessage = error.response.data.message || errorMessage;
+      }
+      throw new Error(errorMessage);
+    }
+  },
+
+  
